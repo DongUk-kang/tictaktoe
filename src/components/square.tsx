31acfa4c4322fd06363b843e59a08060b92c75d1
donguk <gusrn1255@naver.com>
2021-11-05 11:19:02 +0900

    add Square Component
    11.5 오전 11:15기준 추후 업데이트 기준 예정

---
+++ src/components/square.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export type SquareProp = {
+  value: any;
+  onClick: () => void;
+};
+/**
+ * SquareProp 라는 객체 타입을 지정
+ * value는 any
+ * onClick이벤트는 초기 타입선언시 이벤트리스너 타입으로 지정하였으나
+ * 2차로 void로 수정 진행 "onClick: void;" << 해당 선언진행시 타입에러 발생으로인해
+ * arrow func 추가하여 에러 수정
+ *
+ * value 선언한 any타입 수정예정 11.5일 오전 11시15분 기준
+ */
+
+function Square(props: SquareProp) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+/**
+ * 함수명 Square로 지정
+ * 프로퍼티는 위 객체 타입 SquareProp 적용
+ */
+
+export default Square;
